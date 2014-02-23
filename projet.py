c0b5296eddada68c32ecf92f0d91d785e73d7ab5
Lucas Randazzo <randazzo.lucas@gmail.com>
2014-02-23 22:50:00 +0100

    recursion

--- projet.py
+++ projet.py
@@ -9,22 +9,25 @@ import pickle
 #taille_groupe : taille des groupes de mots clefs voulus
 #n : nombre de groupes de mots clefs choisis par vidéo, parmis lesquels la sélection sera faite
 #p : nombre min de groupes de mots clefs voulus
-def mots_clefs_videos(videos,taille_groupes,n,seuil):
+def mots_clefs_videos(videos,taille_groupes,n,seuil,resultats):
     descriptions = []
     mots = []
     for v in videos:
-        name = preparation(v.name.encode("utf8"),[],taille_groupes)
+        name = preparation(v.name.encode("utf8"),resultats,taille_groupes)
         mots.extend(name)
-        desc = preparation(v.desc.encode("utf8"),[],taille_groupes)
+        desc = preparation(v.desc.encode("utf8"),resultats,taille_groupes)
         descriptions.append(desc)
     mots_clefs_descriptions = mots_clefs(descriptions,n,False)
     for l in mots_clefs_descriptions:
         for m in l:
             mots.append(m)
-    return mots_clefs([mots],seuil,True)
+    if taille_groupes == 1:
+        return resultats+mots_clefs([mots],seuil,True)[0]
+    else:
+        return mots_clefs_videos(videos,taille_groupes - 1,n,seuil,resultats+mots_clefs([mots],seuil,True)[0])
 
 file = open("result_MrKuluW","r")
 resultat_parse = pickle.load(file)
 print('fichier chargé')
-print mots_clefs_videos(resultat_parse.playlists["watchHistory"],3,4,0.001)[0]
+print mots_clefs_videos(resultat_parse.playlists["watchHistory"],3,4,0.001,[])
 
