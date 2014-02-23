b95fae35ce2fccc6db483819a95b1294f2794b17
joseph <grosxor@users.noreply.github.com>
2014-02-23 22:26:08 +0100

    avec une fréquence seuil

--- motsclefs.py
+++ motsclefs.py
@@ -4,6 +4,8 @@ import math
 
 separateurs = ".,;:/?!&\n\\\"\'()[]{}#-|"
 
+mots_a_retirer = ['http','www','youtube','com']
+
 mots_filtres = ['le','la','les','un','une','des','de','du','d','l','m','t','s','mais','ou','et','douc','or','ni','car','qui','que','quoi','dont','où','à','dans','par','pour','en','vers','avec','sans','sur','sous','entre','derrière','devant','en','je','tu','il','elle','nou','vous','ils','elles','son','sa','ses','mon','ma','mes','au','ton','ta','tes','notre','nos','votre','vos','leur','leurs','ai','as','a','avons','avez','ont','eu','suis','es','est','sommes','êtes','sont','été','plus','moins','moi','lui','toi','me','te','se','eux','and','by','http','https','www','com','youtube','to','of','the','for','you','on','in','it','is','with','this','my','from','your','are','if','how','org','can','out','one','will','that','user','video','watch','like','upload','was','but','now','have','our','there','some','all']
 
 #mots : liste de chaines
@@ -68,6 +70,8 @@ def simplifie(texte,chaines_a_retirer):
         texte2 = texte2.replace(x,' ')
     for x in chaines_a_retirer:
         texte2 = texte2.replace(x,' ')
+    for x in mots_a_retirer:
+        texte2 = texte2.replace(x,' ')
     return texte2
 
 #texte : chaine
@@ -95,24 +99,33 @@ def preparation(texte,chaines_a_retirer,n):
 
 
 #tfidfs : liste de dict chaine -> coeff
-def mots_frequents(tfidfs,n):
+def mots_frequents(tfidfs,n,seuil):
     resultat = []
     for poids in tfidfs:
         mots = poids.items()
         mots.sort(lambda x,y:cmp(x[1],y[1]),None,True)
         resultat_partiel = []
-        i=0
-        while i<len(mots) and len(resultat_partiel)<n:
-            p= mots[i][1]
-            while i<len(mots) and mots[i][1] == p:
+        i = 0
+        if seuil:
+            while i<len(mots) and mots[i][1]>=n:
                 resultat_partiel.append(mots[i][0])
                 i += 1
+        else:
+            while i<len(mots) and len(resultat_partiel)<n:
+                p= mots[i][1]
+                while i<len(mots) and mots[i][1] == p:
+                    resultat_partiel.append(mots[i][0])
+                    i += 1
         resultat.append(resultat_partiel)
     return resultat
 
 
 #textes : liste de listes de chaines
-def mots_clefs(textes,n):
-    tfidfs = tfidf(textes)
-    return mots_frequents(tfidfs,n)
+def mots_clefs(textes,n,seuil):
+    tfs = []
+    if len(textes)==1:
+        tfs = tf(textes)
+    else:
+        tfs = tfidf(textes)
+    return mots_frequents(tfs,n,seuil)
 
--- projet.py
+++ projet.py
@@ -9,7 +9,7 @@ import pickle
 #taille_groupe : taille des groupes de mots clefs voulus
 #n : nombre de groupes de mots clefs choisis par vidéo, parmis lesquels la sélection sera faite
 #p : nombre min de groupes de mots clefs voulus
-def mots_clefs_videos(videos,taille_groupes,n,p):
+def mots_clefs_videos(videos,taille_groupes,n,seuil):
     descriptions = []
     mots = []
     for v in videos:
@@ -17,14 +17,14 @@ def mots_clefs_videos(videos,taille_groupes,n,p):
         mots.extend(name)
         desc = preparation(v.desc.encode("utf8"),[],taille_groupes)
         descriptions.append(desc)
-    mots_clefs_descriptions = mots_clefs(descriptions,n)
+    mots_clefs_descriptions = mots_clefs(descriptions,n,False)
     for l in mots_clefs_descriptions:
         for m in l:
             mots.append(m)
-    return mots_clefs([mots],p)
+    return mots_clefs([mots],seuil,True)
 
 file = open("result_MrKuluW","r")
 resultat_parse = pickle.load(file)
 print('fichier chargé')
-print(mots_clefs_videos(resultat_parse.playlists["watchHistory"],3,3,6))
+print mots_clefs_videos(resultat_parse.playlists["watchHistory"],3,4,0.001)[0]
 
